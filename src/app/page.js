da3541e0724057a380a19a850ce2914a3368634f
sobuj54 <sobujahmed124@gmail.com>
2025-08-19 15:13:15 +0600

    added cta section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import CtaSection from "./components/CtaSection";
 import WhyTravel from "./components/WhyTravel";
 
 export default function Home() {
   return (
     <>
       <WhyTravel />
+      <CtaSection />
     </>
   );
 }
