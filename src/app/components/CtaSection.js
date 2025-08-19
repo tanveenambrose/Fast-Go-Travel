a194c7c167104eece0d05fdc4b6be818f993ea40
sobuj54 <sobujahmed124@gmail.com>
2025-08-19 19:21:13 +0600

    added frequenty asked questions component

--- src/app/components/CtaSection.js
+++ src/app/components/CtaSection.js
@@ -3,7 +3,7 @@ import { FaApple, FaGooglePlay } from "react-icons/fa";
 
 export default function CtaSection() {
   return (
-    <section className="relative bg-blue-600 rounded-xl overflow-hidden  px-6 sm:px-12 md:px-20 text-white flex flex-row items-center justify-between py-28 mt-10 mb-20 max-w-7xl mx-auto">
+    <section className="relative bg-blue-600 rounded-xl overflow-hidden  px-3 sm:px-12 md:px-20 text-white flex flex-row items-center justify-between py-28 mt-10 mb-20 max-w-7xl mx-auto">
       {/* Phone images */}
       <div className="lg:w-1/2"></div>
       <div className="hidden lg:flex lg:absolute bottom-0 left-1/6">
@@ -27,14 +27,14 @@ export default function CtaSection() {
           <h2 className="text-3xl lg:text-6xl font-bold mb-6">
             Get Started with FastGo Today
           </h2>
-          <div className="flex space-x-4">
-            <button className="flex items-center border border-white rounded-lg px-4 py-2 hover:bg-white hover:text-blue-600 transition">
+          <div className="flex flex-col md:flex-row gap-4">
+            <button className="flex items-center border border-white rounded-lg px-4 py-2 hover:bg-white hover:text-blue-600 transition ">
               <FaApple />
               <span className="ml-2 text-sm font-semibold">
                 Get it on App Store
               </span>
             </button>
-            <button className="flex items-center border border-white rounded-lg px-4 py-2 hover:bg-white hover:text-blue-600 transition">
+            <button className="flex items-center border border-white rounded-lg px-4 py-2 hover:bg-white hover:text-blue-600 transition ">
               <FaGooglePlay />
               <span className="ml-2 text-sm font-semibold">
                 Get it on Google Play
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import CtaSection from "./components/CtaSection";
+import FAQSection from "./components/FaqSection";
 import WhyTravel from "./components/WhyTravel";
 
 export default function Home() {
   return (
     <>
+      <FAQSection />
       <WhyTravel />
       <CtaSection />
     </>
