e2de2808616b93ca7f0642cc26cda5ac5ed4e21a
sobuj54 <sobujahmed124@gmail.com>
2025-08-19 14:01:31 +0600

    added footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
+import Footer from "./components/footer";
+import Header from "./components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,10 +22,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-[1440px] mx-auto overflow-hidden`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-[1440px] mx-auto overflow-x-visible`}
       >
         <Header />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
