6338b9ee6b88b815c73c40bed4d7785e54320805
sobuj54 <sobujahmed124@gmail.com>
2025-08-20 22:44:54 +0600

    header import fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono, DM_Sans } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/footer";
-import Header from "./components/header";
+import Header from "./components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
