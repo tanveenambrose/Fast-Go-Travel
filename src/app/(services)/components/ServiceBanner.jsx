19316abf194f7ad016d405ad70fbdb61f9421350
sobuj54 <sobujahmed124@gmail.com>
2025-09-08 21:21:06 +0600

    tour page completed

--- src/app/(services)/components/ServiceBanner.jsx
+++ src/app/(services)/components/ServiceBanner.jsx
@@ -2,7 +2,7 @@ import TabSystem from "./TabSystem";
 
 const ServiceBanner = () => {
   return (
-    <section className=" py-14 lg:py-20">
+    <section className="py-14 lg:py-20">
       <TabSystem />
     </section>
   );
--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -10,18 +10,18 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
+          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 cursor-pointer",
         destructive:
-          "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+          "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60 cursor-pointer",
         outline:
-          "border bg-background shadow-xs hover:bg-black hover:text-white dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+          "border bg-background shadow-xs hover:bg-black hover:text-white dark:bg-input/30 dark:border-input dark:hover:bg-input/50 cursor-pointer",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
+          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80 cursor-pointer",
         ghost:
-          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
-        link: "text-primary underline-offset-4 hover:underline",
-        blue: "border shadow-xs border-blue-400 hover:bg-blue-500 hover:text-white dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
-        input: "border",
+          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 cursor-pointer",
+        link: "text-primary underline-offset-4 hover:underline cursor-pointer",
+        blue: "border shadow-xs border-blue-400 hover:bg-blue-500 hover:text-white dark:bg-input/30 dark:border-input dark:hover:bg-input/50 cursor-pointer",
+        input: "border cursor-pointer",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
