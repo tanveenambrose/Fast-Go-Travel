cad77dbfcf574d3093c979f3c93d38f155095686
sobuj54 <sobujahmed124@gmail.com>
2025-09-20 21:38:32 +0600

    added booking form,cotraveler,booking summary to cruise booking page

--- src/app/(services)/cruise/booking/page.jsx
+++ src/app/(services)/cruise/booking/page.jsx
@@ -1,5 +1,19 @@
+import AddCotraveler from "../components/AddCotraveler";
+import BookingForm from "../components/BookingForm";
+import CruiseBookingSummary from "../components/CruiseBookingSummary";
+
 const Booking = () => {
-  return <div>Booking page</div>;
+  return (
+    <div className="max-w-[1440px] mx-auto grid grid-cols-1 lg:grid-cols-10 gap-10">
+      <div className="col-span-1 lg:col-span-7 my-7 md:my-14">
+        <BookingForm />
+        <AddCotraveler />
+      </div>
+      <div className="col-span-1 lg:col-span-3 my-7 md:mt-40">
+        <CruiseBookingSummary />
+      </div>
+    </div>
+  );
 };
 
 export default Booking;
