7709ad83b1714a314d34ee7e851c6b8bdc438786
emonkhan500 <khdilo47@gmail.com>
2025-10-02 12:38:47 +0600

    complex slider on all dashboard complete

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -23,13 +23,14 @@ import { RiProfileLine } from "react-icons/ri";
 const layout = ({ children }) => {
   return (
     <div className="relative">
-      <img className="h-52 w-full absolute" src="/images/dashtop.png" alt="" />
-      <div className=" flex flex-col md:flex-row min-h-screen gap-8 px-1 xl:px-26">
+      <img
+        className="h-auto lg:h-52 w-full absolute"
+        src="/images/dashtop.png"
+        alt=""
+      />
+      <div className="flex flex-col lg:flex-row min-h-screen gap-4 px-1 xl:px-20">
         {/* Sidebar */}
-        <aside
-          className="w-auto xl:w-96 bg-white shadow-md mt-4
-     lg:mt-20 p-2 z-10 rounded"
-        >
+        <aside className="w-auto lg:w-72 xl:w-96 bg-white shadow-md mt-4 lg:mt-20 p-2 z-10 rounded">
           <div className="p-6 border-b border-gray-100">
             <h1 className="text-2xl font-semibold text-[#161616] mb-1">
               Mr. Lorem Ipsum
@@ -37,7 +38,7 @@ const layout = ({ children }) => {
             <p className=" text-[#161616]">Joined Since 2025</p>
           </div>
 
-          <div className="p-4 space-y-2">
+          <div className="p-2 space-y-2">
             {/* Account Information - Active */}
             <Link href="/dashboard/account">
               <div className="flex items-start p-4 rounded-lg cursor-pointer transition-colors bg-blue-50 border-l-4 border-blue-500">
@@ -57,7 +58,7 @@ const layout = ({ children }) => {
 
             {/* Your Bookings */}
             <Link href="/dashboard/bookings">
-              <div className="flex items-start p-4 rounded-lg cursor-pointer transition-colors hover:bg-gray-50">
+              <div className="flex items-start p-2 rounded-lg cursor-pointer transition-colors hover:bg-gray-50">
                 <div className="flex-shrink-0 w-8 h-8 flex items-center justify-center mr-3">
                   <FaCalendarAlt className="text-gray-600 text-xl" />
                 </div>
@@ -74,7 +75,7 @@ const layout = ({ children }) => {
 
             {/* Co-Travelers */}
             <Link href="/dashboard/travelers">
-              <div className="flex items-start p-4 rounded-lg cursor-pointer transition-colors hover:bg-gray-50">
+              <div className="flex items-start p-2 rounded-lg cursor-pointer transition-colors hover:bg-gray-50">
                 <div className="flex-shrink-0 w-8 h-8 flex items-center justify-center mr-3">
                   <FaUsers className="text-gray-600 text-xl" />
                 </div>
@@ -91,7 +92,7 @@ const layout = ({ children }) => {
 
             {/* Promo Codes/Coupons */}
             <Link href="/dashboard/promocodes">
-              <div className="flex items-start p-4 rounded-lg cursor-pointer transition-colors hover:bg-gray-50">
+              <div className="flex items-start p-2 rounded-lg cursor-pointer transition-colors hover:bg-gray-50">
                 <div className="flex-shrink-0 w-8 h-8 flex items-center justify-center mr-3">
                   <FaTicketAlt className="text-gray-600 text-xl" />
                 </div>
@@ -108,7 +109,7 @@ const layout = ({ children }) => {
 
             {/* Settings */}
             <Link href="/dashboard/settings">
-              <div className="flex items-start p-4 rounded-lg cursor-pointer transition-colors hover:bg-gray-50">
+              <div className="flex items-start p-2 rounded-lg cursor-pointer transition-colors hover:bg-gray-50">
                 <div className="flex-shrink-0 w-8 h-8 flex items-center justify-center mr-3">
                   <FaCog className="text-gray-600 text-xl" />
                 </div>
@@ -124,7 +125,7 @@ const layout = ({ children }) => {
             </Link>
 
             {/* Log Out */}
-            <div className="flex items-start p-4 rounded-lg cursor-pointer transition-colors hover:bg-gray-50">
+            <div className="flex items-start p-2 rounded-lg cursor-pointer transition-colors hover:bg-gray-50">
               <div className="flex-shrink-0 w-8 h-8 flex items-center justify-center mr-3">
                 <FaSignOutAlt className="text-gray-600 text-xl" />
               </div>
@@ -134,68 +135,61 @@ const layout = ({ children }) => {
             </div>
           </div>
         </aside>
+
+        {/* Right Side */}
         <div className="flex flex-col flex-1">
           {/* common slider */}
-          <div className="z-10 mt-6 lg:mt-20 relative">
-            <Swiper
-              navigation={{
-                nextEl: ".nextBtn",
-                prevEl: ".prevBtn",
-              }}
-              breakpoints={{
-                319: { slidesPerView: 1 },
-                480: { slidesPerView: 1 },
-                640: { slidesPerView: 2 },
-                768: { slidesPerView: 2 },
-                992: { slidesPerView: 2 },
-                1024: { slidesPerView: 3 },
-                1280: { slidesPerView: 4 },
-                1440: { slidesPerView: 4 },
-              }}
-              mousewheel={true}
-              keyboard={true}
-              spaceBetween={10}
-              modules={[Navigation, Mousewheel, Keyboard]}
-              className="mySwiper max-w-6xl  relative "
-            >
-              {/* 1 */}
-              <SwiperSlide className="">
-                <img src="/images/item.png" alt="" />
-              </SwiperSlide>
+          <div className="z-10 mt-6 lg:mt-20 relative px-0 xl:px-12">
+            <div className="relative w-full lg:max-w-2xl xl:max-w-4xl 2xl:max-w-6xl mx-auto">
+              <Swiper
+                navigation={{
+                  nextEl: ".nextBtn",
+                  prevEl: ".prevBtn",
+                }}
+                breakpoints={{
+                  319: { slidesPerView: 1 },
+                  480: { slidesPerView: 1 },
+                  640: { slidesPerView: 2 },
+                  768: { slidesPerView: 2 },
+                  992: { slidesPerView: 2 },
+                  1024: { slidesPerView: 3 },
+                  1280: { slidesPerView: 3 },
+                  1440: { slidesPerView: 4 },
+                }}
+                mousewheel={true}
+                keyboard={true}
+                spaceBetween={10}
+                modules={[Navigation, Mousewheel, Keyboard]}
+                className="mySwiper"
+              >
+                {/* Slides */}
+                <SwiperSlide>
+                  <img src="/images/item.png" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src="/images/item1.png" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src="/images/item2.png" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src="/images/item.png" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src="/images/item1.png" alt="" />
+                </SwiperSlide>
+              </Swiper>
 
-              {/* 2 */}
-              <SwiperSlide className=" ">
-                <img src="/images/item1.png" alt="" />
-              </SwiperSlide>
-              {/* 3 */}
-              <SwiperSlide className=" ">
-                <img src="/images/item2.png" alt="" />
-              </SwiperSlide>
-              {/* 3 */}
-              <SwiperSlide className=" ">
-                <img src="/images/item.png" alt="" />
-              </SwiperSlide>
-              {/* 3 */}
-              <SwiperSlide className=" ">
-                <img src="/images/item1.png" alt="" />
-              </SwiperSlide>
-            </Swiper>
-
-            <div>
-              <div className="relative z-20 flex gap-8 xl:gap-0 justify-center md:justify-between  xl:max-w-[1250px] mx-auto ">
-                <div className=" md:-mt-20 ">
-                  <button className="group  hover:bg-[#232F3F] prevBtn border border-[#232F3F]  p-2  rounded-full text-[#232F3F] ">
-                    <FaArrowLeft className="group-hover:text-white" />
-                  </button>
-                </div>
-                <div className="md:-mt-20 ">
-                  <button className="group  hover:bg-[#232F3F] nextBtn border border-[#232F3F]  p-2  rounded-full text-[#232F3F]">
-                    <FaArrowRight className="group-hover:text-white" />
-                  </button>
-                </div>
-              </div>
+              {/* Arrows outside swiper */}
+              <button className="prevBtn absolute top-1/2 -left-0 lg:-left-6 transform -translate-y-1/2 z-30 border border-[#232F3F] p-2 rounded-full text-[#232F3F] hover:bg-[#232F3F] group bg-white">
+                <FaArrowLeft className="group-hover:text-white" />
+              </button>
+              <button className="nextBtn absolute top-1/2 -right-0 lg:-right-6 transform -translate-y-1/2 z-30 border border-[#232F3F] p-2 rounded-full text-[#232F3F] hover:bg-[#232F3F] group bg-white">
+                <FaArrowRight className="group-hover:text-white" />
+              </button>
             </div>
           </div>
+
           {/* Main Content */}
           <main className="flex-1 mt-10 lg:mt-40 ">{children}</main>
         </div>
